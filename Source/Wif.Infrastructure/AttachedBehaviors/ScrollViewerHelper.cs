df5ebc0180633aa19d28306d69a444ce69feadd3
LeoYang <leolion610@outlook.com>
2019-04-03 23:08:00 +0800

    Add AttachedBehaviors and wif.xmind

--- Source/Wif.Infrastructure/AttachedBehaviors/ScrollViewerHelper.cs
+++ Source/Wif.Infrastructure/AttachedBehaviors/ScrollViewerHelper.cs
@@ -2,7 +2,7 @@
 using System.Windows.Controls;
 using System.Windows.Input;
 
-namespace Frontier.Wif.Utils.Helpers
+namespace Frontier.Wif.Infrastructure.AttachedBehaviors
 {
     /// <summary>
     /// Defines the <see cref="ScrollViewerHelper" />
