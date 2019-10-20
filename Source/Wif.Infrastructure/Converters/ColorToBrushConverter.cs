6b464be73d23cbf9284822a149afe531dbc8e30b
LeoYang <leolion610@outlook.com>
2019-10-20 16:53:26 +0800

    Add ColorToBrushConverter class.

---
+++ Source/Wif.Infrastructure/Converters/ColorToBrushConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Media;
+using Frontier.Wif.Infrastructure.MarkupExtensions;
+
+namespace Frontier.Wif.Infrastructure.Converters
+{
+    /// <summary>
+    ///     Represents the converter that converts Color values to and from Brush values.
+    /// </summary>
+    public class ColorToBrushConverter : MarkupConverter<ColorToBrushConverter>
+    {
+        /// <summary>
+        ///     Converts a value.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        ///     A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        protected override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var brush = new SolidColorBrush();
+            if (value != null) brush.Color = (Color) value;
+
+            return brush;
+        }
+
+        /// <summary>
+        ///     Converts a value.
+        /// </summary>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        ///     A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        protected override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is SolidColorBrush brush) return brush.Color;
+
+            return null;
+        }
+    }
+}
