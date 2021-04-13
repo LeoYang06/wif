fd689ddb76f241059ec99fa1c3802be554bbca1d
LeoYang <leolion610@outlook.com>
2021-04-13 14:31:09 +0800

    Add the UidAttribute class.

---
+++ Source/Wif.Core/Attributes/UidAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Frontier.Wif.Core.Attributes
+{
+    /// <summary>
+    /// 表示唯一标识的特性。
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All)]
+    public class UidAttribute : Attribute
+    {
+        /// <summary>
+        /// 无参构造函数。
+        /// </summary>
+        public UidAttribute()
+        {
+        }
+
+        /// <summary>
+        /// 有参构造函数。
+        /// </summary>
+        /// <param name="uid">字符串表现形式的唯一标识。</param>
+        public UidAttribute(string uid)
+        {
+            Uid = uid;
+        }
+
+        /// <summary>
+        /// 获取字符串形式的唯一标识。
+        /// </summary>
+        public string Uid { get; }
+    }
+}
