49b9ced7cb6dcfb1055ad4810075a6d51c62de08
LeoYang <leolion610@outlook.com>
2021-08-30 16:09:00 +0800

    Add the InverseBooleanConverter Class.

---
+++ Source/Wif.Infrastructure/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Frontier.Wif.Infrastructure.MarkupExtensions;
+
+namespace Frontier.Wif.Infrastructure.Converters
+{
+    /// <summary>
+    /// 接受一个bool值，并返回其反值。
+    /// </summary>
+    public class InverseBooleanConverter : MarkupConverter
+    {
+        /// <summary>
+        /// 将一个布尔值转换为其反值。
+        /// </summary>
+        /// <param name="value">The source value.</param>
+        /// <param name="targetType">The parameter is not used.</param>
+        /// <param name="parameter">The parameter is not used.</param>
+        /// <param name="culture">The parameter is not used.</param>
+        /// <returns>The inverted boolean value.</returns>
+        protected override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            var boolValue = (bool)value;
+            return !boolValue;
+        }
+
+        /// <summary>
+        /// 将一个布尔值转换为其反值。
+        /// </summary>
+        /// <param name="value">The parameter is not used.</param>
+        /// <param name="targetType">The parameter is not used.</param>
+        /// <param name="parameter">The parameter is not used.</param>
+        /// <param name="culture">The parameter is not used.</param>
+        /// <returns>The parameter is not used.</returns>
+        protected override object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            var boolValue = (bool)value;
+            return !boolValue;
+        }
+    }
+}
